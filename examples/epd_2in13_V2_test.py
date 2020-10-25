31bebeef5b899889066489b18907f5a6e20fa873
Tom Bishop <tjbishop@gmail.com>
2020-10-24 20:00:33 -0700

    update interface name

--- examples/epd_2in13_V2_test.py
+++ examples/epd_2in13_V2_test.py
@@ -95,7 +95,7 @@ try:
     global s
     s = conf.L2socket(iface=newiface)
 
-    nearbyAps = wifi.cell.all('wlan1')
+    nearbyAps = wifi.cell.all('wlan1mon')
     logging.info("epd2in13_V2 Demo")
 
     epd = epd2in13_V2.EPD()
