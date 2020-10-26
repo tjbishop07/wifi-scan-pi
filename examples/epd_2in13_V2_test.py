f3ab269ccc93ed1fc4945df028b792e22c459180
Tom Bishop <tjbishop@gmail.com>
2020-10-26 11:33:10 -0700

    tracker jacker test

--- examples/epd_2in13_V2_test.py
+++ examples/epd_2in13_V2_test.py
@@ -11,6 +11,7 @@ import yaml
 
 import signal
 from scapy.all import *
+import yaml
 
 picdir = os.path.join(os.path.dirname(
     os.path.dirname(os.path.realpath(__file__))), 'pic')
@@ -22,6 +23,7 @@ if os.path.exists(libdir):
 
 logging.basicConfig(level=logging.DEBUG)
 newiface = 'wlan1mon'
+wifi_map_path = 'wifi_map.yaml'
 ap_list = []
 font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
 font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
@@ -39,11 +41,43 @@ def PacketHandler(packet):
                 time_draw.rectangle((0, 0, 220, 105), fill=255)
                 mac_address = packet.addr2
                 ap_ssid = packet.info
-                time_draw.text((0, 50), mac_address, font=font15, fill=0, align="center")
-                time_draw.text((0, 75), ap_ssid.decode('UTF-8'), font=font15, fill=0, align="center")
+                time_draw.text((0, 50), mac_address,
+                               font=font15, fill=0, align="center")
+                time_draw.text((0, 75), ap_ssid.decode('UTF-8'),
+                               font=font15, fill=0, align="center")
                 epd.displayPartial(epd.getbuffer(time_image))
                 print("MAC: %s with SSID: %s " %
                       (packet.addr2, packet.info))
+
+
+def parse_wifi_map(map_path):
+    with open(map_path, 'r') as f:
+        data = f.read()
+
+    wifi_map = yaml.load(data)
+    devices = set()
+    associated_devices = set()
+
+    for ssid in wifi_map:
+        print('ssid = {}'.format(ssid))
+        ssid_node = wifi_map[ssid]
+        for bssid in ssid_node:
+            print('\tbssid = {}'.format(bssid))
+            bssid_node = ssid_node[bssid]
+            if 'devices' in bssid_node:
+                for device in bssid_node['devices']:
+                    devices |= {device}
+                    if ssid != '~unassociated_devices':
+                        associated_devices |= {device}
+                        print('\t\tdevice (associated) = {}'.format(device))
+                    else:
+                        print('\t\tdevice = {}'.format(device))
+
+    time_draw.rectangle((120, 80, 220, 105), fill=255)
+    time_draw.text((120, 80), 'SSID count: {}, Associated device count: {}, Device count: {}'.format(len(wifi_map), len(associated_devices), len(devices))', font=font15, fill=0)
+    epd.displayPartial(epd.getbuffer(time_image))
+    print('\n\nSSID count: {}, Associated device count: {}, Device count: {}'.format(
+        len(wifi_map), len(associated_devices), len(devices)))
 
     # try:
     #     SRCMAC = packet[0].addr2
@@ -168,6 +202,9 @@ try:
     epd.Clear(0xFF)
 
     while (True):
+
+        parse_wifi_map(wifi_map_path)
+
         time_draw.rectangle((120, 80, 220, 105), fill=255)
         time_draw.text((120, 80), time.strftime(
             '%H:%M:%S'), font=font24, fill=0)
