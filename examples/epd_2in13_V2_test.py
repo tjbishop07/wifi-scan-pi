6f28cd0688ee070d5afa2c5dbf35f54e4372cb34
Tom Bishop <tjbishop@gmail.com>
2020-10-26 11:46:20 -0700

    init tj on launch

--- examples/epd_2in13_V2_test.py
+++ examples/epd_2in13_V2_test.py
@@ -76,7 +76,7 @@ def parse_wifi_map(map_path):
     time_draw.rectangle((0, 0, 220, 105), fill=255)
     time_draw.text((0, 0), 'SSID count: {}'.format(
         len(wifi_map)), font=font15, fill=0)
-    time_draw.text((0, 0), 'Associated device count: {}, Device count: {}'.format(
+    time_draw.text((0, 0), 'Associated device count: {}'.format(
         len(associated_devices)), font=font15, fill=0)
     time_draw.text((0, 0), 'Device count: {}'.format(
         len(devices)), font=font15, fill=0)
@@ -161,6 +161,7 @@ try:
     time_draw.rectangle((0, 0, 220, 105), fill=255)
     time_draw.text((0, 0), "Scanning...", font=font15, fill=0)
     epd.displayPartial(epd.getbuffer(time_image))
+    os.system('trackerjacker -i wlan1 --map')
 
     # logging.info("1.Drawing on the image...")
     # image = Image.new('1', (epd.height, epd.width),
@@ -202,15 +203,15 @@ try:
     # epd.display(epd.getbuffer(image1))
     # time.sleep(2)
 
-    #sniff(iface=newiface, prn=PacketHandler)
+    # sniff(iface=newiface, prn=PacketHandler)
     # epd.init(epd.FULL_UPDATE)
     # epd.Clear(0xFF)
 
-    while (True):
+   while (True):
 
         parse_wifi_map(wifi_map_path)
 
-        #time_draw.rectangle((120, 80, 220, 105), fill=255)
+        # time_draw.rectangle((120, 80, 220, 105), fill=255)
         # time_draw.text((120, 80), time.strftime(
         #    '%H:%M:%S'), font=font24, fill=0)
         # epd.displayPartial(epd.getbuffer(time_image))
