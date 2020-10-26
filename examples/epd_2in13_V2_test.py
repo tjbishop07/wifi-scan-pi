c578630045ab2b713e668ea549868437a482a67a
Tom Bishop <tjbishop@gmail.com>
2020-10-25 21:40:17 -0700

    text update

--- examples/epd_2in13_V2_test.py
+++ examples/epd_2in13_V2_test.py
@@ -36,10 +36,9 @@ def PacketHandler(packet):
             if packet.addr2 not in ap_list:
                 ap_list.append(packet.addr2)
                 time_draw.rectangle((0, 0, 220, 105), fill=255)
-                time_draw.text((0, 0), "Access Point MAC: %s with SSID: %s " % (
-                    packet.addr2, packet.info), font=font15, fill=0)
+                time_draw.text((0, 0), "%s (%s) " % (packet.addr2, packet.info), font=font15, fill=0)
                 epd.displayPartial(epd.getbuffer(time_image))
-                print("Access Point MAC: %s with SSID: %s " %
+                print("MAC: %s with SSID: %s " %
                       (packet.addr2, packet.info))
 
     # try:
