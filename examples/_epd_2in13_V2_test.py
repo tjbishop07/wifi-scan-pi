6439b04ba566967a935a1a90f38229c6e1dd478e
Tom Bishop <tjbishop@gmail.com>
2020-10-26 19:53:04 -0700

    update load image

--- examples/_epd_2in13_V2_test.py
+++ examples/_epd_2in13_V2_test.py
@@ -89,8 +89,8 @@ try:
     # time_draw.rectangle((0, 0, 220, 105), fill=255)
     # time_draw.text((0, 0), "Loading...", font=font15, fill=0)
 
-    blackimage1 = Image.new('1', (epd.height // 2, epd.width // 2), 255)  # 298*126
-    newimage = Image.open(os.path.join(picdir, 'terminus-qr.bmp'))
+    blackimage1 = Image.new('1', (epd.height, epd.width), 255)  # 298*126
+    newimage = Image.open(os.path.join(picdir, 'terminus-qr.png'))
     blackimage1.paste(newimage, (0, 0))
     epd.display(epd.getbuffer(blackimage1))
 
