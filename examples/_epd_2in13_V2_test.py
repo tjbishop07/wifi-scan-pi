47b28142c4910ef542b239f4ce4b7786a23c39c5
Tom Bishop <tjbishop@gmail.com>
2020-10-26 20:07:02 -0700

    update

--- examples/_epd_2in13_V2_test.py
+++ examples/_epd_2in13_V2_test.py
@@ -61,7 +61,6 @@ def parse_wifi_map(map_path):
         free = round(disk.free/1024.0/1024.0/1024.0, 1)
         total = round(disk.total/1024.0/1024.0/1024.0, 1)
 
-        epd.Clear(0xFF)
         time_draw.rectangle((0, 0, 220, 125), fill=255)
         time_draw.text((0, 0), 'SSID count: {}'.format(
             len(wifi_map)), font=font15, fill=0)
@@ -87,16 +86,11 @@ try:
     epd.Clear(0xFF)
     num = 0
 
-    # time_draw.rectangle((0, 0, 220, 105), fill=255)
-    # time_draw.text((0, 0), "Loading...", font=font15, fill=0)
-
     blackimage1 = Image.new('1', (epd.height, epd.width), 255)  # 298*126
     newimage = Image.open(os.path.join(picdir, 'terminus-qr.png'))
     blackimage1.paste(newimage, (10, 10))
     epd.display(epd.getbuffer(blackimage1))
 
-    # epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(time_image))
-    # epd.displayPartial(epd.getbuffer(blackimage1))
     #os.system('trackerjacker -i wlan1 --map')
     #logging.info("Launching tJ...")
     #process = subprocess.Popen(['trackerjacker', '-i', 'wlan1', '--map'])
